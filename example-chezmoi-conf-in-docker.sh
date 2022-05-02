bdcb16eb1395977ed90a7bdfc74aee57f098b78d
Hg <dev@indigo.re>
2022-05-02 21:19:40 +0200

    example-chezmoi-conf-in-docker.sh

---
+++ example-chezmoi-conf-in-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -e
+# use a container but miss your carefully crafted dotfiles?
+# if you use chezmoi, it's easy
+
+TMPDIR=${TMPDIR:-/tmp}
+d=$(mktemp "$TMPDIR/chezmoi-docker.XXXXXX")
+
+# create a copy of the dotfiles in a temp directory
+chezmoi archive | tar -x -C "$d"
+
+# mount the temp directory as ~root so root has all the dotfiles
+docker run -it -v "$d:/root" debian:stable bash
