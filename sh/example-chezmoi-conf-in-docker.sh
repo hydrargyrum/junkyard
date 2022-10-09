9318539b2fb0344acc0b69390e8df069f7d4a644
Hg <dev@indigo.re>
2022-10-09 22:32:57 +0200

    sh/example-chezmoi-conf-in-docker.sh: instructions for already running box

--- sh/example-chezmoi-conf-in-docker.sh
+++ sh/example-chezmoi-conf-in-docker.sh
@@ -9,4 +9,11 @@ d=$(mktemp -d "$TMPDIR/chezmoi-docker.XXXXXX")
 chezmoi archive | tar -x -C "$d"
 
 # mount the temp directory as ~root so root has all the dotfiles
+# and start the container
 docker run -it -v "$d:/root" debian:stable bash
+
+# alternatively, to copy dotfiles to an already running container:
+#  chezmoi archive | docker exec -u root -w /root -i CONTAINER_ID tar xf -
+
+# or (if not using root user in the container):
+#  chezmoi archive | docker exec -u foo -w /home/foo -i CONTAINER_ID tar xf -
