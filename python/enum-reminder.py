3dd67e48e4af50cb51f76053763f9a0e355538f8
Hg <dev@indigo.re>
2023-06-18 11:20:51 +0200

    python/enum-reminder.py: rework + list()

--- python/enum-reminder.py
+++ python/enum-reminder.py
@@ -7,24 +7,38 @@ class E(enum.Enum):
     UPPER = "lower"
 
 
-### repr vs str
+print("### repr vs str")
 
+print("repr(E.UPPER): ", end="")
 print(repr(E.UPPER))
 # prints: <E.UPPER: 'lower'>
 
+print("str(E.UPPER): ", end="")
 print(str(E.UPPER))
 # prints: E.UPPER
 
-### promote value to enum
+print("### enum entry fields")
 
+print("repr(E.UPPER.name): ", end="")
+print(repr(E.UPPER.name))
+# prints: 'UPPER'
+
+print("repr(E.UPPER.value): ", end="")
+print(repr(E.UPPER.value))
+# prints: 'lower'
+
+print("### promote value to enum")
+
+print('repr(E("lower")): ', end="")
 print(repr(E("lower")))
 # prints: <E.UPPER: 'lower'>
 
+print('repr(E["UPPER"]): ', end="")
 print(repr(E["UPPER"]))
 # prints: <E.UPPER: 'lower'>
 
-### enum entry fields
+print("### listing entries")
 
-assert E.UPPER.name == 'UPPER'
-
-assert E.UPPER.value == 'lower'
+print("list(E): ", end="")
+print(list(E))
+# prints: [<E.UPPER: 'lower'>]
