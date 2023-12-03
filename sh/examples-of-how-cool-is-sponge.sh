38bca50f5a22f52f165a1645a8d268c290fe8453
Hg <dev@indigo.re>
2023-12-03 20:48:19 +0100

    sh/examples-of-how-cool-is-sponge.sh: warn about empty file caveat

--- sh/examples-of-how-cool-is-sponge.sh
+++ sh/examples-of-how-cool-is-sponge.sh
@@ -17,6 +17,11 @@ expand -t4 somefile.py | sponge somefile.py
 # indent a json file in-place:
 python3 -m json.tool somefile.json | sponge somefile.json
 
-# interactive sed, display results only when the input is finished
+# sed on TTY, display results only when the input is finished
 # so input and output are distinguishable in terminal display:
 sed 's/some/thing/g' somepattern | sponge
+
+# WARNING: if the "producer" command fails, sponge still overwrites the file
+awk '{ print $2 ' some-file.txt | sponge some-file.txt
+# oops syntax error in awk expression: awk quits without printing anything
+# oops sponge wrote an empty some-file.txt, destroying the file!
